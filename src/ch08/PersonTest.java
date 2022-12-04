ebee491f22119b49b3462ed048a289aece93a7ef
starMinK <rbals040329@gmail.com>
2022-12-05 07:15:33 +0900

    Chapter2 ch20~ch24 Chapter3 ch01~ch02

--- src/ch08/PersonTest.java
+++
@@ -1,9 +0,0 @@
-package ch08;
-
-public class PersonTest {
-    public static void main(String[] args) {
-        Person personMinsu = new Person(180, 78, "남성", "Tomas", 37);
-
-        System.out.println(personMinsu.personInfo());
-    }
-}
